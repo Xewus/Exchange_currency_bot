aa08266bc1df1849055ce8f4695f78dc8fc7838f
Xewus <xewuss@yandex.ru>
2021-11-28 18:40:36 +0300

    v.1.2

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# go_skill
-intensive homework
+# Currency store
+
+The telegram bot in which you can add your cryptocurrency
+and count the balance in real money.
+
+# Examples of command
+Add cryptocurrency and/or increase balance:
+- command currency number
+...
+ADD BTC 9.999
+...
+
+Decrease balance:
+- command currency number
+...
+SUB ETH 7.777
+...
+
+Remove cryptocurrency:
+- command currency
+...
+DEL BTC
+...
+
+Show your balance in real money:
+- command [option RUB, GBP, EUR etc, default USD]
+...
+SHOW
+...
+SHOW EUR
+...
--- main.go
+++ main.go
@@ -4,28 +4,32 @@ import (
 	"encoding/json"
 	"errors"
 	. "fmt"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
 // output text
-const any_error = "some mistake has occurred"
-const incorrect_format_input = "Incorrect command format!"
-const error_coversation = "Conversion error!"
-const for_add = "Added: %s %f. Balance: %f"
-const for_cut = "Subtracted: %s %f. Balance: %f"
-const for_del = "Values for %s cleaned. Balance: %f"
-const for_show = "- for %s: %f"
-const not_zero_code = "Incorrect currency"
-const no_currency = "Incorrect currency"
+//const any_error = "some mistake has occurred"
+const incorrectFormatInput = "Incorrect command format!"
+const errorCoversation = "Conversion error!"
+const forAdd = "Added: %s %f. Balance: %f."
+const forSub = "Subtracted: %s %f. Balance: %f."
+const forDel = "Values for %s cleaned."
+const forShow = "- for %s: %f.  (%.2f %s)\n"
+const forShowSum = "Summa in %s: %.2f"
+const notZeroCode = "Incorrect currency."
+const noCurrency = "Incorrect currency."
+const noCurrencyInWallet = "This currency is not in your wallet."
+const urlBinance = "https://www.binance.com/api/v3/ticker/price?symbol=%s%s"
 
 type binanceResp struct {
 	Price float64 `json:"price,string"`
-	Code int64 `json:"code"`
+	Code  int64   `json:"code"`
 }
-
 type wallet map[string]float64
 
 var db = map[int64]wallet{}
@@ -35,14 +39,10 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
@@ -50,73 +50,129 @@ func main() {
 			continue
 		}
 		var text string
+		var isInWallet bool
+		chatId := update.Message.Chat.ID
 		msgArr := strings.Split(update.Message.Text, " ")
 
+		if _, ok := db[update.Message.Chat.ID]; !ok {
+			db[chatId] = wallet{}
+		}
 		switch msgArr[0] {
 		case "ADD":
-			err := checkInput(msgArr)
+			_, err = checkInput(chatId, msgArr, 3, true)
 			if err != nil {
-				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
+				bot.Send(tgbotapi.NewMessage(chatId, err.Error()))
 				continue
 			}
 			summa, err := strconv.ParseFloat(msgArr[2], 64)
 			if err != nil {
-				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, error_coversation))
+				bot.Send(tgbotapi.NewMessage(chatId, errorCoversation))
 				continue
 			}
-			if _, ok := db[update.Message.Chat.ID]; !ok {
-				db[update.Message.Chat.ID] = wallet{}
-			}
-				db[update.Message.Chat.ID][msgArr[1]] += summa
-			text = Sprintf(for_add, msgArr[1], summa, db[update.Message.Chat.ID][msgArr[1]])
+			db[chatId][msgArr[1]] += summa
+			text = Sprintf(forAdd, msgArr[1], summa, db[chatId][msgArr[1]])
 		case "SUB":
-
-			err := checkInput(msgArr)
+			isInWallet, err := checkInput(chatId, msgArr, 3, false)
 			if err != nil {
-				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
+				bot.Send(tgbotapi.NewMessage(chatId, err.Error()))
+				continue
+			}
+			if !isInWallet {
+				bot.Send(tgbotapi.NewMessage(chatId, noCurrencyInWallet))
 				continue
 			}
 			summa, err := strconv.ParseFloat(msgArr[2], 64)
-			if err != nil{
-				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, error_coversation))
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, errorCoversation))
 				continue
 			}
-			db[update.Message.Chat.ID][msgArr[1]] -= summa
-			text = Sprintf(for_cut, msgArr[1], summa, db[update.Message.Chat.ID][msgArr[1]])
+			db[chatId][msgArr[1]] -= summa
+			text = Sprintf(forSub, msgArr[1], summa, db[chatId][msgArr[1]])
 		case "DEL":
-			db[update.Message.Chat.ID][msgArr[1]] = 0
-			text = Sprintf(for_del, msgArr[1], db[update.Message.Chat.ID][msgArr[1]])
+			isInWallet, err = checkInput(chatId, msgArr, 2, false)
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(chatId, err.Error()))
+				continue
+			}
+			if !isInWallet {
+				bot.Send(tgbotapi.NewMessage(chatId, noCurrencyInWallet))
+				continue
+			}
+			delete(db[chatId], msgArr[1])
+			text = Sprintf(forDel, msgArr[1])
 		case "SHOW":
-			var priceUsd, sumRub, usd float64
+			money := "USDT"
+			_, err = checkInput(chatId, msgArr, 2, false)
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(chatId, err.Error()))
+				continue
+			}
+
+			var price, summa float64
+			if len(msgArr) == 2 {
+				money = msgArr[1]
+			}
 			text = "Balance:\n"
-			for key, value := range db[update.Message.Chat.ID] {
-				priceUsd, err = getPrice(key)
+			for key, value := range db[chatId] {
+				price, err = getPrice(key, money)
 				if err != nil {
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
+					bot.Send(tgbotapi.NewMessage(chatId, err.Error()))
 					continue
 				}
-				usd = value * priceUsd
-				sumRub += usd
-				text += (Sprintf(for_show, key, value) + Sprintf(" (%.2f RUB)\n", usd))
-		    }
-			text += Sprintf("Summa: %.2f RUB", sumRub)
+				price *= value
+				summa += price
+				text += Sprintf(forShow, key, value, price, money)
+			}
+			text += Sprintf(forShowSum, money, summa)
 		default:
 			text = "I don't know this command." + update.Message.Text
 		}
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-
+		msg := tgbotapi.NewMessage(chatId, text)
 		bot.Send(msg)
 	}
 }
 
-func checkInput(arr []string) (err error) {
-	if (arr[0] == "ADD" || arr[0] == "SUB") && len(arr) != 3 {
-		err = errors.New(incorrect_format_input)
+func checkInput(chatId int64, arr []string, needLen int, isCurrency bool) (isInWallet bool, err error) {
+	switch arr[0] {
+	case "SHOW":
+		if len(arr) > needLen {
+			err = errors.New(incorrectFormatInput)
+			return
+		}
+		if len(arr) == 1 {
+			return
+		}
 		return
+	default:
+		if len(arr) != needLen {
+			err = errors.New(incorrectFormatInput)
+			return
+		}
 	}
+	_, isInWallet = db[chatId][arr[1]]
+	if isCurrency {
+		var resp *http.Response
+		resp, err = http.Get(Sprintf(urlBinance, arr[1], "USDT"))
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
 
-	resp, err := http.Get(Sprintf("https://www.binance.com/api/v3/ticker/price?symbol=%sUSDT", arr[1]))
+		var jsonResp binanceResp
+		err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+		if err != nil {
+			return
+		}
+		if jsonResp.Code != 0 {
+			err = errors.New("noCurrency")
+			return
+		}
+	}
+	return
+}
+
+func getPrice(coin, money string) (price float64, err error) {
+	resp, err := http.Get(Sprintf(urlBinance, coin, money))
 	if err != nil {
 		return
 	}
@@ -128,43 +184,9 @@ func checkInput(arr []string) (err error) {
 		return
 	}
 	if jsonResp.Code != 0 {
-		err = errors.New(no_currency)
-		return
-	}
-	return
-}
-
-func  getPrice(coin string) (price float64, err error) {
-	respUsd, err :=http.Get(Sprintf("https://www.binance.com/api/v3/ticker/price?symbol=%sUSDT", coin))
-	if err != nil {
-		return
-	}
-	respRub, err :=http.Get("https://www.binance.com/api/v3/ticker/price?symbol=USDTRUB")
-	if err != nil {
-		return
-	}
-	defer respUsd.Body.Close()
-	defer respRub.Body.Close()
-
-	var jsonResp binanceResp
-	err = json.NewDecoder(respUsd.Body).Decode(&jsonResp)
-	if err != nil {
-		return
-	}
-	if jsonResp.Code != 0 {
-		err = errors.New(not_zero_code)
+		err = errors.New(notZeroCode)
 		return
 	}
 	price = jsonResp.Price
-
-	err = json.NewDecoder(respRub.Body).Decode(&jsonResp)
-	if err != nil {
-		return
-	}
-	if jsonResp.Code != 0 {
-		err = errors.New(not_zero_code)
-		return
-	}
-	price *= jsonResp.Price
 	return
 }
